8c11e988cb95c5932dad034cec434185af8f23c3
ashishgangwar07 <ashishgangwar07@gmail.com>
2022-09-08 16:59:28 +0530

    created seperate routing

--- bookStoreApp/src/app/app-routing.module.ts
+++ bookStoreApp/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AboutUsComponent } from "./about-us/about-us.component";
-import { AuthComponent } from "./auth/auth.component";
-import { LoginComponent } from "./auth/components/login/login.component";
-import { SignupComponent } from "./auth/components/signup/signup.component";
+// import { AuthComponent } from "./auth/auth.component";
+// import { LoginComponent } from "./auth/components/login/login.component";
+// import { SignupComponent } from "./auth/components/signup/signup.component";
 
 const routes: Routes=[
     {path:'about-us', component: AboutUsComponent},
-    {path:'auth', component:AuthComponent, children:[
-        {path:'login', component:LoginComponent},
-        {path:'signup', component:SignupComponent}
-    ]}
+    // {path:'auth', component:AuthComponent, children:[
+    //     {path:'login', component:LoginComponent},
+    //     {path:'signup', component:SignupComponent}
+    // ]}
 ];
 
 @NgModule({
--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
+// import { RouterModule, Routes } from '@angular/router';
+// import { AboutUsComponent } from './about-us/about-us.component';
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { PublicModule } from './public/public.module';
+import { UserModule } from './user/user.module';
 
 // const routes: Routes =[
 //   {path:'about-us', component: AboutUsComponent}
@@ -19,7 +20,8 @@ import { PublicModule } from './public/public.module';
    //RouterModule.forRoot(routes)
    AppRoutingModule,
    AuthModule,
-   PublicModule
+   PublicModule,
+   UserModule
   ],
  // exports:[RouterModule],
   providers: [],
