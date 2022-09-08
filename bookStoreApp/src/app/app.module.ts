1fed73b97b855707e71b0d518e154f00eb40efeb
ashishgangwar07 <ashishgangwar07@gmail.com>
2022-09-08 16:29:15 +0530

    added routing in application

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { PublicModule } from './public/public.module';
 
+// const routes: Routes =[
+//   {path:'about-us', component: AboutUsComponent}
+// ]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   //RouterModule.forRoot(routes)
+   AppRoutingModule,
+   AuthModule,
+   PublicModule
   ],
+ // exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
